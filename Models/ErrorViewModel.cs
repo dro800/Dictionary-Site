fe0f813cec72e2057cdbf242fdf4e1a07d879951
Daniel Olvera <danielolvera311@gmail.com>
2023-04-27 16:55:18 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Dictionary_Site.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
